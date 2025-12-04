b415e9395399d4c8dc601e44fd35df33c2743034
Bogdan_Off <rumkry123@gmail.com>
2025-12-04 15:08:50 +0100

    Fix the blacklist issue

--- src/main/java/mod/cdv/mixin/quark/client/IButtonProviderMixin.java
+++
@@ -1,9 +0,0 @@
-package mod.cdv.mixin.quark.client;
-
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.violetmoon.quark.api.IQuarkButtonAllowed;
-
-@Mixin(AbstractContainerScreen.class)
-public class IButtonProviderMixin implements IQuarkButtonAllowed { }
---
+++ src/main/java/mod/cdv/mixin/quark/client/InventoryButtonHandlerMixin.java
@@ -0,0 +1,30 @@
+package mod.cdv.mixin.quark.client;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.gui.screens.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.violetmoon.quark.api.IQuarkButtonAllowed;
+import org.violetmoon.quark.base.client.handler.InventoryButtonHandler;
+
+@Mixin(value = InventoryButtonHandler.class, remap = false)
+public abstract class InventoryButtonHandlerMixin {
+    @Definition(id = "screen", local = @Local(index = 2))
+    @Definition(id = "IQuarkButtonAllowed", type = IQuarkButtonAllowed.class)
+    @Expression("screen instanceof IQuarkButtonAllowed")
+    @ModifyExpressionValue(
+            method = "initGui",
+            at = @At("MIXINEXTRAS:EXPRESSION")
+    )
+    private static boolean forciblyAddApocScreens(boolean original, @Local(name = "screen") Screen screen) {
+        // Package classpath contains all screens from Apoc
+        // Now makes quark add buttons even if the config is set to whitelist and doesn't include the Apoc screens
+        if(screen.getClass().getName().startsWith("net.mcreator.apocalypsenow.client.gui."))
+            return true;
+        else
+            return original;
+    }
+}
